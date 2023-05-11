eb2d96e4ad7cd08f5a2bf5b8f910deae4df91580
seuyon0101 <seuyon0101@gmail.com>
2023-05-11 13:20:23 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# saturi_lab_multi_nmt_low_resource
-Multi Neural Machine Translation for English to Korean Dialect
+# Multi Neural Machine Translation for English to Korean Dialect
+---
+### 모두의 연구소 saturi-lab
+
+**sauturi lab 은 영어에서 5개 지방 방언(충청, 제주, 강원, 경상, 전라)으로 번역하는 모델(multi NMT)에 대한 연구를 수행하고 논문을 작성하는 것을 목표로하는 모임입니다.**
+
+**이 모임은 Low-Resource Language(LRL)와 관련된 one-to-many translation 방법을 탐색하고 가설을 세우고 실험을 진행합니다.**
+
+**프로젝트는 우리나라 언어 보존에 도움을 줄 수 있어 사회적인 가치가 있고 한국어 LRL 연구에 필요성과 발전을 위해 기여할 수 있는 기회가 될 수 있습니다.**
+
+---
+@ 본프로젝트는 모두의연구소와 고용노동부 'K-디지털 플랫폼' 사업의 지원을 받아 만들었습니다. 
+
---
+++ preprocessing/eng_translation_cleaning.py
@@ -0,0 +1,73 @@
+if __name__ == '__main__' :
+
+	import pandas as pd
+	import tqdm
+	import re
+	from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+	from torch.utils.data import DataLoader
+
+	# load data
+
+	df = pd.read_csv('dataset_full_v1.csv') # data found on google drive 'raw' directory
+
+	#data cleaning
+
+	df['text']=df['text'].apply(lambda x : re.sub('[\(\)]','',str(x))) # remove parenthesis
+	df['text']=df['text'].apply(lambda x : re.sub('(\&+[a-zA-Zㄱ-힣\&]+)','',str(x))) # remove words like &&name, &company name, etc.
+	df['text']=df['text'].apply(lambda x : re.sub('(\@+[ㄱ-힣\@]+)','',str(x))) # remove words like &&name, &company name, etc.
+	df['text']=df['text'].apply(lambda x : re.sub('(x+[a-zㄱ-힣]+)','',str(x))) # remove words like xx하면, xxxx, etc
+	df['text']=df['text'].apply(lambda x : re.sub('(\-+[a-zㄱ-힣\-]+)','',str(x))) # remove words like --하면, -시사- etc
+	df['text']=df['text'].apply(lambda x : re.sub(r'(.+)\1{2,}',r'\1',str(x))) # remove repeating words
+	df['text']=df['text'].apply(lambda x : re.sub('([ㄱ-힣]+)\~$',r'\1.',str(x))) # sub words ending in ~ to . ex 그럴거야~ 그럴거야.
+	df['text']=df['text'].apply(lambda x : re.sub('([ㄱ-힣]+)\~','',str(x))) # remove expressions like 어~ 아~ 으~ 오~
+
+	df['dial']=df['dial'].apply(lambda x : re.sub('[\(\)]','',str(x)))
+	df['dial']=df['dial'].apply(lambda x : re.sub('(\&+[a-zA-Zㄱ-힣\&]+)','',str(x)))
+	df['dial']=df['dial'].apply(lambda x : re.sub('(\@+[ㄱ-힣\@]+)','',str(x))) # remove words like @이름,@주소,@상호명, etc.
+	df['dial']=df['dial'].apply(lambda x : re.sub('(x+[a-zㄱ-힣]+)','',str(x))) # remove words like xx하면, xxxx, etc
+	df['dial']=df['dial'].apply(lambda x : re.sub('(\-+[a-zㄱ-힣\-\&]+)','',str(x))) # remove words like --하면, -시사- etc
+	df['dial']=df['dial'].apply(lambda x : re.sub(r'(.+)\1{2,}',r'\1',str(x))) # remove repeating words
+	df['dial']=df['dial'].apply(lambda x : re.sub('([ㄱ-힣]+)\~$',r'\1.',str(x))) # sub words ending in ~ to . ex 그럴거야~ 그럴거야.
+	df['dial']=df['dial'].apply(lambda x : re.sub('([ㄱ-힣]+)\~','',str(x))) # remove expressions like 어~ 아~ 으~ 오~
+
+	#remove from training
+
+	df.loc[df['text'].str.contains('/'),'rm'] = 1 #containing mislabelled data
+	df['len'] = df['text'].apply(lambda x: len(x))
+	df.loc[df['len'] < 2,'rm'] = 1 #length is less than 2
+	df.drop_duplicates(subset='dial', inplace=True) # drop duplicated senteces in dial
+	clean_df = df.loc[df['rm']!=1,['text','dial','reg','pair']]
+
+	# translation pipeline
+
+	ckpt = 'circulus/kobart-trans-ko-en-v2'
+	tokenizer = AutoTokenizer.from_pretrained(ckpt)
+	model = AutoModelForSeq2SeqLM.from_pretrained(ckpt)
+	translator = pipeline("translation", model=ckpt)
+
+	text = clean_df.text.values.tolist()
+	dataloader = DataLoader(text,  batch_size=512)
+    
+    #save translations
+	outputs = []
+	for n,i in enumerate(tqdm.tqdm(dataloader)) :
+	    inputs = tokenizer(i, return_tensors="pt", padding=True).input_ids
+	    out = model.generate(inputs,num_beams=3, max_new_tokens=60) # do_sample=True, top_k=30, top_p=0.95,
+	    for j in out :
+	        result = tokenizer.decode(j, skip_special_tokens=True)
+	        outputs.append(result)
+	    
+	    if n % 100 == 0 : #save every 100th batch
+	        with open('translated.txt','w') as f :
+	            for sen in outputs :
+	                f.write(sen+'\n')
+   
+    # save all remaining outputs                 
+	with open('translated.txt','r') as f :
+        raw = f.read().splitlines()
+
+    #add translated english list as new column
+	clean_df['eng'] = raw
+    
+    # export to csv
+	clean_df.to_csv('translated_train_data.csv')
